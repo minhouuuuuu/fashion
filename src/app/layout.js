39ad16c2839868882f8e68079d329d7780637bbf
NGUYEN Minh <minh.nguyen@ddth.corp>
2023-10-05 13:54:57 +0200

    add google search console

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="qmzXgCX8nKLiRUaSWY0vaJt5CK_CQRLXH8oCO3l_L1k" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
