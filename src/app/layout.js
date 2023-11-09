0f5d2776c6402e7eba13c18e4a0026bf25ad1439
Minh <bao-minh.nguyen@etu.unistra.fr>
2023-11-09 09:54:59 +0100

    clean up project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Fashion show',
-  description: 'Stunning fashion website',
-}
+    title: "Art",
+    description: "Stunning art website",
+    viewport: "width=device-width, initial-scale=1",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta name="google-site-verification" content="qmzXgCX8nKLiRUaSWY0vaJt5CK_CQRLXH8oCO3l_L1k" />
-      </head>
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head>
+                <meta
+                    name="google-site-verification"
+                    content="qmzXgCX8nKLiRUaSWY0vaJt5CK_CQRLXH8oCO3l_L1k"
+                />
+            </head>
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
