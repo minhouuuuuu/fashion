83577525a9d8db0d72081e7561fd9e36c1bfe4fd
Minh <bao-minh.nguyen@etu.unistra.fr>
2023-11-09 15:57:36 +0100

    clean up project

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,18 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
     title: "Art",
     description: "Stunning art website",
-    viewport: "width=device-width, initial-scale=1",
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <head>
-                <meta
-                    name="google-site-verification"
-                    content="qmzXgCX8nKLiRUaSWY0vaJt5CK_CQRLXH8oCO3l_L1k"
-                />
-            </head>
             <body className={inter.className}>{children}</body>
         </html>
     );
